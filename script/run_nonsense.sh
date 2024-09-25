9f3f1881a0ab45dbda88688f476fa79e051ac74b
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2024-09-25 17:17:53 +0000

    little script to run check_nonsense_mutations.py

--- script/run_nonsense.sh
+++ script/run_nonsense.sh
@@ -1,11 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+    echo "run_nonsense.sh - identify nonsense mutations"
+    echo "Usage: run_nonsense.sh <NUCLEOTIDE_FASTA>"
+    exit 1
+fi
 
 script_dir=`dirname $0`
 DATABASE="$script_dir/database/references.faa"
-tmpdir=`mktemp`
+tmpdir=`mktemp -d`
 
 
-my $input_fasta=$1
+input_fasta="$1"
 
 blastx -query $input_fasta -subject $DATABASE -out $tmpdir/blast.xml -outfmt 5
 
