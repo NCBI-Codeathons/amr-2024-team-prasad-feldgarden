98423ec4ac1ba535fb66ac8f035bcc8f10767198
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2024-09-25 20:17:58 +0000

    Added "run_dgw.sh" to run diamond then DGW.py

---
+++ script/run_dgw.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+    echo "run_dgw.sh - identify nonsense mutations and frame shifts"
+    echo "Usage: run_dgw.sh <NUCLEOTIDE_FASTA>"
+    exit 1
+fi
+
+script_dir=`dirname $0`
+DATABASE="$script_dir/database/references.faa"
+db_base=`basename $DATABASE .faa`
+db_dir=`dirname $DATABASE`
+if [ ! -e "$db_dir/$db_base.dmnd" ]
+then
+    diamond makedb --in $DATABASE --db "$db_dir/$db_base"
+fi
+tmpdir=`mktemp -d`
+
+input_fasta="$1"
+
+diamond blastx --query $input_fasta --db $DATABASE --out $tmpdir/blast.xml --frameshift 15 --min-orf 1 -f 5
+
+micromamba run -n codeathon $script_dir/DGW.py -i $tmpdir/blast.xml
+
+
+rm -r $tmpdir
+
