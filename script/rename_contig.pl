2a304093273875cf5fcb650bb018b47bb2eeaf8b
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2024-09-26 20:00:24 +0000

    Added rename_contig.pl to add assembly accessions
    Quick and dirty script

---
+++ script/rename_contig.pl
@@ -0,0 +1,31 @@
+#!/bin/env perl
+use strict;
+use warnings;
+use File::Basename;
+
+if (@ARGV == 0) {
+    print "rename_contig.pl - rename contigs to add the assembly accession to the\n";
+    print "start of the contig identifier\n";
+    print 'Uses s/(\.\d+)_.*/$1/; to identify the assembly accession from the filename';
+    print "\nUsage: rename_contig.pl <FASTA_FILE> [<FASTA_FILE> ...]\n";
+    exit 1;
+}
+
+my @files = @ARGV;
+
+foreach my $file (@files) {
+    open(my $fh, "<", $file)
+        or die "Couldn't open $file: $!";
+    open(my $out, ">", "$file.renamed")
+        or die "Couldn't open $file: $!";
+    my $base = basename($file, '_genomic.dgw');
+    $base =~ s/(\.\d+)_.*/$1/;
+
+    while (<$fh>) {
+        if (/^>/) {
+            $_ =~ s/^>/>${base}-/;
+        }
+        print $out $_;
+    }
+}
+
