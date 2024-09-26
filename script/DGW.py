a2f9fad9c1faaa06f1c6f4f6fe00365eae84129b
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2024-09-26 10:39:24 -0600

    add indels option to turn on in-frame mutations

--- script/DGW.py
+++ script/DGW.py
@@ -58,7 +58,7 @@ def main(argv):
                 coverage = (hsp.sbjct_end - hsp.sbjct_start + 1 )/alignment.length 
                 identity = hsp.identities/hsp.align_length
                 if coverage > argv.cov and identity > argv.id : 
-                    indels_mutations = check_INDELs(query_seq,hit_seq, hsp.sbjct_start)
+                    indels_mutations = check_INDELs(query_seq,hit_seq, hsp.sbjct_start) if argvs.indels else []
                     nonsense_mutations = check_nonsense_mutations(query_seq)
                     frameshift_mutations = check_frameshift(query_seq)
                     
@@ -86,16 +86,27 @@ def main(argv):
                                             str(f"{hsp.sbjct_end - hsp.sbjct_start + 1}/{alignment.length} ({coverage * 100:.2f}%)"), 
                                             ])
                                 )
+    result_handle.close()
 
 if __name__ == '__main__':
     toolname = os.path.basename(__file__)
     argv = argparse.ArgumentParser( prog=toolname,
-        description = "check nonsensus/frameshift mutations from blastx result xml file",
-        formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+        description = r'''
+
+    ____                 _    ____                  __        __    _ _    _             
+   |  _ \  ___  __ _  __| |  / ___| ___ _ __   ___  \ \      / __ _| | | _(_)_ __   __ _ 
+   | | | |/ _ \/ _` |/ _` | | |  _ / _ | '_ \ / _ \  \ \ /\ / / _` | | |/ | | '_ \ / _` |
+   | |_| |  __| (_| | (_| | | |_| |  __| | | |  __/   \ V  V | (_| | |   <| | | | | (_| |
+   |____/ \___|\__,_|\__,_|  \____|\___|_| |_|\___|    \_/\_/ \__,_|_|_|\_|_|_| |_|\__, |
+                                                                                   |___/ 
+    Check nonsensus/frameshift mutations from blastx result xml file
+    ''',
+        formatter_class = argparse.RawDescriptionHelpFormatter)
     argv.add_argument('-i', '--input', dest = 'input_xml', required = True,
         help = '-outfmt 5 -o blastx_out.xml')
     argv.add_argument('--cov', type=float, dest = 'cov', default = .8 , required=False, help='target coverage')
     argv.add_argument('--id',  type=float, dest = 'id',  default = .8 , required=False, help='hit identity')
+    argv.add_argument('--indels', action='store_true', help='check in-frame insertion and deletion')
     argv.add_argument('--verbose', action='store_true', help='Show more information in log')
     argv.add_argument('--version', action='version', version='%(prog)s v{version}'.format(version=__version__))
 
