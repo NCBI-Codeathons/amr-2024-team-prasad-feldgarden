8d03b1766852971242d0d1c6631460cc3b8366e7
Ana Ramos <anaramos.data@gmail.com>
2024-10-15 16:50:38 -0400

    updating SQL model and correcting calculations in analysis for randomset2

--- results/script_comparison/dgw_combined_outputs.sql
+++ results/script_comparison/dgw_combined_outputs.sql
@@ -1,5 +1,6 @@
 .mode csv
 .output results/script_comparison/dgw_combined_outputs_randomset2.csv
+
 WITH 
 -- Replace the DGW.py output file in each union block as required
 dgw_combined_p AS (
@@ -87,11 +88,11 @@ r_mutations AS (
   ON all_acc.asm_acc = dgw_combined_r.asm_acc
 ),
 
-combined_0 AS (
+combined AS (
   SELECT DISTINCT
     all_acc.asm_acc,
-    p_mutations.p_mutations_concat,
-    r_mutations.r_mutations_concat,
+    -- p_mutations.p_mutations_concat,
+    -- r_mutations.r_mutations_concat,
     p_mutations.p_element_symbol,
     r_mutations.r_element_symbol,
     p_mutations.p_lesion,
@@ -104,83 +105,49 @@ combined_0 AS (
     p_mutations.p_mut_count,
     r_mutations.r_mut_count
   FROM all_acc
-  Left JOIN p_mutations
+  LEFT JOIN p_mutations
   ON all_acc.asm_acc = p_mutations.asm_acc
   LEFT JOIN r_mutations
   ON all_acc.asm_acc = r_mutations.asm_acc
-  --AND (p_mutations.p_mutations_concat = r_mutations.r_mutations_concat
-  --OR LOWER(p_mutations.p_element_symbol) IS NULL)
-  ORDER BY all_acc.asm_acc, p_mut_count
-),
+  AND p_mutations.p_mutations_concat = r_mutations.r_mutations_concat
+  WHERE p_mut_count > 0
 
-combined AS (
-  SELECT 
-    asm_acc,
-    p_mutations_concat,
-    r_mutations_concat,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_element_symbol
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_element_symbol
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN p_element_symbol
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_element_symbol
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-    END AS p_element_symbol,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_element_symbol
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_element_symbol
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_element_symbol
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN r_element_symbol
-    END AS r_element_symbol,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_lesion
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_lesion
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN p_lesion
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_lesion
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-    END AS p_lesion,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_lesion
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_lesion
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_lesion
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN r_lesion
-    END AS r_lesion,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_lession_type
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_lession_type
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN p_lession_type
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_lession_type
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-    END AS p_lession_type,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_lession_type
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_lession_type
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_lession_type
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN r_lession_type
-    END AS r_lession_type,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_mutation_id
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_mutation_id
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN p_mutation_id
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN p_mutation_id
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-    END AS p_mutation_id,
-    CASE
-      WHEN p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_mutation_id 
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_mutation_id
-      WHEN p_mut_count > r_mut_count AND p_mutations_concat != r_mutations_concat THEN NULL
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat = r_mutations_concat THEN r_mutation_id
-      WHEN p_mut_count < r_mut_count AND p_mutations_concat != r_mutations_concat THEN r_mutation_id
-    END AS r_mutation_id,
-    p_mut_count,
-    r_mut_count,
-    -- cohort
-  FROM combined_0
-  WHERE (p_mut_count = r_mut_count AND p_mutations_concat = r_mutations_concat)
-  OR p_mut_count != r_mut_count
-),
+  UNION BY NAME
+
+  SELECT DISTINCT
+    all_acc.asm_acc,
+    r_mutations.r_element_symbol,
+    r_mutations.r_lesion,
+    r_mutations.r_lession_type,
+    r_mutations.r_mutation_id,
+    -- STRING_AGG(DISTINCT(p_mutations.cohort, r_mutations.cohort)) OVER (PARTITION BY all_acc.asm_acc) AS cohort,
+    r_mutations.r_mut_count
+  FROM all_acc
+  LEFT JOIN p_mutations
+  ON all_acc.asm_acc = p_mutations.asm_acc
+  LEFT JOIN r_mutations
+  ON all_acc.asm_acc = r_mutations.asm_acc
+  WHERE p_mutations.p_mutations_concat != r_mutations.r_mutations_concat
+  AND p_mutations.p_mut_count > 0
+  AND LOWER(r_element_symbol) NOT IN (SELECT DISTINCT LOWER(p_element_symbol) FROM p_mutations WHERE asm_acc = r_mutations.asm_acc)
+
+  UNION BY NAME
+
+  SELECT DISTINCT
+    all_acc.asm_acc,
+    r_mutations.r_element_symbol,
+    r_mutations.r_lesion,
+    r_mutations.r_lession_type,
+    r_mutations.r_mutation_id,
+    -- STRING_AGG(DISTINCT(p_mutations.cohort, r_mutations.cohort)) OVER (PARTITION BY all_acc.asm_acc) AS cohort,
+    r_mutations.r_mut_count
+  FROM all_acc
+  LEFT JOIN p_mutations
+  ON all_acc.asm_acc = p_mutations.asm_acc
+  LEFT JOIN r_mutations
+  ON all_acc.asm_acc = r_mutations.asm_acc
+  WHERE p_mut_count = 0
+) , 
 
 mbe AS (
   SELECT 
@@ -198,55 +165,75 @@ mbe AS (
 
 combined_2 AS (
   SELECT DISTINCT
-      combined.asm_acc,
-      combined.p_element_symbol,
-      combined.r_element_symbol,
-      mbe.mb_element_symbol,
-      CASE 
-        WHEN LOWER(mbe.mb_element_symbol) IN (LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) THEN 1
-        WHEN COALESCE(LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) IS NULL AND mbe.mb_element_symbol IS NOT NULL THEN 1
-        WHEN mbe.mb_element_symbol IS NOT NULL THEN 0
-        ELSE 3
-      END AS mb_element_symbol_check,
-      combined.p_lesion,
-      combined.r_lesion,
-      mbe.mb_lesion,
-      combined.p_lession_type,
-      combined.r_lession_type,
-      combined.p_mutation_id,
-      combined.r_mutation_id,
-      -- cohort
+    combined.asm_acc,
+    combined.p_element_symbol,
+    combined.r_element_symbol,
+    mbe.mb_element_symbol,
+    CASE 
+      WHEN LOWER(mbe.mb_element_symbol) IN (LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) THEN 1
+      WHEN COALESCE(LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) IS NULL AND mbe.mb_element_symbol IS NOT NULL THEN 1
+      WHEN mbe.mb_element_symbol IS NOT NULL THEN 0
+      ELSE 3
+    END AS mb_element_symbol_check,
+    combined.p_lesion,
+    combined.r_lesion,
+    mbe.mb_lesion,
+    combined.p_lession_type,
+    combined.r_lession_type,
+    combined.p_mutation_id,
+    combined.r_mutation_id,
+    -- cohort
   FROM combined
   LEFT JOIN mbe
   ON combined.asm_acc = mbe.asm_acc
+  AND LOWER(mbe.mb_element_symbol) IN (LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol))
+  QUALIFY COUNT(COALESCE(combined.p_element_symbol, combined.r_element_symbol)) OVER (PARTITION BY combined.asm_acc) > 0
+
+  UNION BY NAME
+
+  SELECT DISTINCT
+    combined.asm_acc,
+    mbe.mb_element_symbol,
+    CASE 
+      WHEN LOWER(mbe.mb_element_symbol) IN (LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) THEN 1
+      WHEN COALESCE(LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) IS NULL AND mbe.mb_element_symbol IS NOT NULL THEN 1
+      WHEN mbe.mb_element_symbol IS NOT NULL THEN 0
+      ELSE 3
+    END AS mb_element_symbol_check,
+    mbe.mb_lesion
+    -- cohort
+  FROM combined
+  LEFT JOIN mbe
+  ON combined.asm_acc = mbe.asm_acc
+  QUALIFY COUNT(COALESCE(combined.p_element_symbol, combined.r_element_symbol)) OVER (PARTITION BY combined.asm_acc) = 0
+    UNION BY NAME
+
+  SELECT DISTINCT
+    combined.asm_acc,
+    mbe.mb_element_symbol,
+    CASE 
+      WHEN LOWER(mbe.mb_element_symbol) IN (LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) THEN 1
+      WHEN COALESCE(LOWER(combined.p_element_symbol), LOWER(combined.r_element_symbol)) IS NULL AND mbe.mb_element_symbol IS NOT NULL THEN 1
+      WHEN mbe.mb_element_symbol IS NOT NULL THEN 0
+      ELSE 3
+    END AS mb_element_symbol_check,
+    mbe.mb_lesion
+    -- cohort
+  FROM combined
+  LEFT JOIN mbe
+  ON combined.asm_acc = mbe.asm_acc
+  WHERE mbe.mb_element_symbol IS NOT NULL
+  AND LOWER(mbe.mb_element_symbol) NOT IN (SELECT DISTINCT LOWER(p_element_symbol) FROM combined WHERE asm_acc = mbe.asm_acc)
+  AND LOWER(mbe.mb_element_symbol) NOT IN (SELECT DISTINCT LOWER(r_element_symbol) FROM combined WHERE asm_acc = mbe.asm_acc)
+  QUALIFY COUNT(COALESCE(combined.p_element_symbol, combined.r_element_symbol)) OVER (PARTITION BY combined.asm_acc) > 0
 ),
 
 final AS (
 SELECT 
-  *
-FROM combined_2
-WHERE mb_element_symbol_check != 0
-
-UNION BY NAME
-
-SELECT 
-  * EXCLUDE(mb_element_symbol, mb_lesion)
-FROM combined_2
-WHERE mb_element_symbol_check = 0
-
-UNION BY NAME
-
-SELECT 
-  asm_acc,
-  mb_element_symbol,
-  mb_lesion
-FROM combined_2
-WHERE mb_element_symbol_check = 0
-)
-SELECT 
-  * EXCLUDE(mb_element_symbol_check),
+  *,
   md5(CONCAT(asm_acc, p_element_symbol, r_element_symbol, mb_element_symbol, p_lesion, r_lesion, mb_lesion, p_lession_type, r_lession_type, p_mutation_id, r_mutation_id)) AS record_id,  
-FROM final
-ORDER BY asm_acc;
+FROM combined_2
+ORDER BY asm_acc
+) 
 
- 
+SELECT * FROM final;
